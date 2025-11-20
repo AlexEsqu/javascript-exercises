133908fef65a8fe2c2ab959a99011ccd9abcc2a4
Alex Kling <momiji.kling@gmail.com>
2025-11-21 00:44:18 +0100

    Do ex09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+	if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)
+		|| Number.isNaN(a) || Number.isNaN(b))
+		return 'ERROR';
+
+
+	let result = 0;
+	let start = a < b ? a : b;
+	let end = a < b ? b : a;
+
+	while (start <= end)
+	{
+		result += start;
+		start++;
+	}
+
+	return result;
 };
 
 // Do not edit below this line
