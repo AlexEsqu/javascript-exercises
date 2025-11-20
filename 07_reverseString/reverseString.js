18dd19efae4bad018ea64fd8adfb3ed5b50ef1d9
Alex Kling <momiji.kling@gmail.com>
2025-11-20 23:27:43 +0100

    Do ex07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	let end = str.length - 1;
+	let result = "";
+	for (let begin = 0; begin < str.length; begin++)
+	{
+		result += str[end];
+		end--;
+	}
+	return result;
 };
 
 // Do not edit below this line
