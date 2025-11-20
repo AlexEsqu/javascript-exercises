b43f405f5de2c62091d6b3eac69998c035304b09
Alex Kling <momiji.kling@gmail.com>
2025-11-21 00:35:16 +0100

    Do ex08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elemsToRemove) {
+	let result = array;
+	for (const elemToRemove of elemsToRemove)
+		result = result.filter((elem) => elem !== elemToRemove);
+	return result;
 };
 
 // Do not edit below this line
