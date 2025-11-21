906f04b17a8136aeddeda21c4432830c76dbf9a4
Alex <momiji.kling@gmail.com>
2025-11-21 18:57:16 +0100

    Do ex14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
+	if (target < 0)
+		return 'OOPS';
+	if (target == 0)
+		return 0;
+	if (target == 1 || target == 2)
+		return 1;
 
+	let fi = [0, 1, 1];
+
+	for (let i = 3; i <= target; i++)
+	{
+		fi.push(fi[i - 2] + fi[i - 1]);
+	}
+	return fi[target];
 };
 
 // Do not edit below this line
