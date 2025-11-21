358d943c0dcc4aed7c6c5e1c964ec51619eacd81
Alex Kling <momiji.kling@gmail.com>
2025-11-21 09:54:05 +0100

    Do ex11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let result =  (tempF - 32) * (5 / 9);
+  if (Number.isInteger(result))
+    return result;
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let result = tempC * (9/5) + 32;
+  if (Number.isInteger(result))
+    return result;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
