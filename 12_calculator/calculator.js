610568b3fedae858cdbaf8fec58b2218983f6462
Alex Kling <momiji.kling@gmail.com>
2025-11-21 10:08:44 +0100

    Do ex12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(add, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array[0];
+  for (num of array.splice(1))
+    result *= num;
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let f = [];
+  if (num == 0 || num == 1)
+    return 1;
+  if (f[num] > 0)
+      return f[num];
+  return f[num] = factorial(num-1) * num;
 };
 
 // Do not edit below this line
