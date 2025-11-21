dcc20ad728800a3f661fa14e9ed50a10374694dd
Alex <momiji.kling@gmail.com>
2025-11-21 12:01:28 +0100

    Do ex13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+	let cleanWord = word.replace(/[^A-Za-z0-9]/g, '').toUpperCase();
+	let right = cleanWord.length - 1;
+	let middle = cleanWord.length / 2;
+	console.log(cleanWord);
+
+	for (let left = 0; left < middle; left++, right--)
+	{
+		console.log(cleanWord[left] + " and " + cleanWord[right]);
+		if (cleanWord[left] != cleanWord[right])
+			return false;
+	}
+	return true;
 };
 
 // Do not edit below this line
