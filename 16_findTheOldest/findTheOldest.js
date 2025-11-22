eef353e23f253751a2ee9f1ee181f460ff008377
Alex <momiji.kling@gmail.com>
2025-11-22 13:20:11 +0100

    Do ex16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const getAgeOfPerson = function(person){
+	const age = person.yearOfDeath ? person.yearOfBirth - person.yearOfDeath : person.yearOfBirth - 2025;
+	return age;
+}
 
+const getoldestOfBoth = function(person1, person2) {
+	return getAgeOfPerson(person1) < getAgeOfPerson(person2) ? person1 : person2;
+}
+
+const findTheOldest = function(map) {
+	return map.reduce(getoldestOfBoth, map[0]);
 };
 
 // Do not edit below this line
